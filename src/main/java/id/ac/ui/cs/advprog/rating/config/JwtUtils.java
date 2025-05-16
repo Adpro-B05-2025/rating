0620fa144fee1b29b6fbf2e3ece6b5d5887565cf
nsrnannaisha <nisrinaannaisha@gmail.com>
2025-05-16 17:26:33 +0700

    [GREEN] Modify Jwt Utils

---
+++ src/main/java/id/ac/ui/cs/advprog/rating/config/JwtUtils.java
@@ -0,0 +1,52 @@
+package id.ac.ui.cs.advprog.rating.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.SignatureException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtils {
+
+    private final String jwtSecret;
+
+    public JwtUtils(@Value("${pandacare.app.jwtSecret}") String jwtSecret) {
+        this.jwtSecret = jwtSecret;
+        System.out.println("DEBUG: JWT secret in rating service = " + jwtSecret);
+    }
+
+    private Key getSigningKey() {
+        if (jwtSecret == null) {
+            throw new IllegalStateException("JWT secret key is not initialized");
+        }
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUserIdFromJwtToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e) {
+            System.out.println("Invalid JWT signature: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Invalid JWT token: " + e.getMessage());
+        }
+        return false;
+    }
+}
