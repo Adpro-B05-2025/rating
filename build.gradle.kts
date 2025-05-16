dcbba34188817c8aedbd7a7f8cf534ad5036777a
nsrnannaisha <nisrinaannaisha@gmail.com>
2025-05-16 16:21:07 +0700

    [GREEN] Modify db, jwt configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.postgresql:postgresql:42.7.3")
+    implementation("io.github.cdimascio:dotenv-java:2.2.4")
 
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
--- src/main/java/id/ac/ui/cs/advprog/rating/RatingApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/rating/RatingApplication.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.rating;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RatingApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        System.setProperty("PASSWORD_DB", dotenv.get("PASSWORD_DB"));
+
         SpringApplication.run(RatingApplication.class, args);
     }
 
