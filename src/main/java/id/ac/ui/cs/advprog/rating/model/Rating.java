5534c05afe7ed5823b819ad7cb4d9b55b745f8a6
nsrnannaisha <nisrinaannaisha@gmail.com>
2025-05-16 10:29:35 +0700

    [REFACTOR] Change database

--- src/main/java/id/ac/ui/cs/advprog/rating/model/Rating.java
+++ src/main/java/id/ac/ui/cs/advprog/rating/model/Rating.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "rating", schema = "rating")
+@Table(name = "rating")
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
