6e195229ffa08954aa2540eb6894b33bcee5414b
nsrnannaisha <nisrinaannaisha@gmail.com>
2025-05-25 19:44:09 +0700

    [GREEN] Improve app test

--- src/main/java/id/ac/ui/cs/advprog/rating/RatingApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/rating/RatingApplication.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.rating;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RatingApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-        System.setProperty("PASSWORD_DB", dotenv.get("PASSWORD_DB"));
-
-        SpringApplication.run(RatingApplication.class, args);
+      SpringApplication.run(RatingApplication.class, args);
     }
 
 }
--- src/test/java/id/ac/ui/cs/advprog/rating/RatingApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/rating/RatingApplicationTests.java
@@ -1,13 +1,33 @@
 package id.ac.ui.cs.advprog.rating;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RatingApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void mainMethodShouldCallSpringApplicationRun() {
+        try (MockedStatic<SpringApplication> mocked = Mockito.mockStatic(SpringApplication.class)) {
+            mocked.when(() -> SpringApplication.run(eq(RatingApplication.class), any(String[].class)))
+                    .thenReturn(null);
+
+            RatingApplication.main(new String[]{});
+
+            mocked.verify(() -> SpringApplication.run(eq(RatingApplication.class), any(String[].class)), Mockito.times(1));
+        }
+    }
 }
