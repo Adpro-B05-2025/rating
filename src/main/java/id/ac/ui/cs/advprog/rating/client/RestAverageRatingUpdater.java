23b429e4e346346f1040ad5fd1015e506310c4cd
nsrnannaisha <nisrinaannaisha@gmail.com>
2025-05-25 21:07:47 +0700

    [REFACTOR] Implement average rating updater

---
+++ src/main/java/id/ac/ui/cs/advprog/rating/client/RestAverageRatingUpdater.java
@@ -0,0 +1,32 @@
+package id.ac.ui.cs.advprog.rating.client;
+
+import id.ac.ui.cs.advprog.rating.service.AverageRatingUpdater;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class RestAverageRatingUpdater implements AverageRatingUpdater {
+
+    private final RestTemplate restTemplate;
+    private final String authProfileBaseUrl = "http://localhost:8081/api/caregiver";
+
+    public RestAverageRatingUpdater(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public void updateAverageRating(Long doctorId) {
+        String url = authProfileBaseUrl + "/" + doctorId + "/averageRating";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>("", headers);
+
+        restTemplate.exchange(url, HttpMethod.PATCH, entity, String.class);
+    }
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/rating/service/AverageRatingUpdater.java
@@ -0,0 +1,5 @@
+package id.ac.ui.cs.advprog.rating.service;
+
+public interface AverageRatingUpdater {
+    void updateAverageRating(Long doctorId);
+}
