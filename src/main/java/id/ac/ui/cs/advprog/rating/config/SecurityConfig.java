b2d3733beb393a73d190a6c286e2412c2ea11914
nsrnannaisha <nisrinaannaisha@gmail.com>
2025-06-05 01:15:09 +0700

    trial cors

--- src/main/java/id/ac/ui/cs/advprog/rating/config/SecurityConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/rating/config/SecurityConfig.java
@@ -52,14 +52,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("https://lucent-treacle-8937a6.netlify.app"));
+        configuration.setAllowedOrigins(Arrays.asList("https://lucent-treacle-8937a6.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
-        configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
-
-        System.out.println("CORS config loaded!");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
